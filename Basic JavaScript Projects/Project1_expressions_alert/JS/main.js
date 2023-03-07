133ed302a5f483e2b893e4f90cf6bff7756ab1e7
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-07 14:07:45 -0800

    //

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,10 +1,10 @@
 var myString ="HELLLLOOO WORLD!!!";
-window.alert(myString);
+window.alert(myString); // diplaying variable thats assigned as window alert 
 
-var firstName="Angel";
-var lastName= "Velasco";
-var fullName= firstName + " " + lastName;
+var firstName="Angel"; // Defining a variable and giving it a string value
+var lastName= "Velasco"; // Defining a variable and giving it a string value
+var fullName= firstName + " " + lastName;  // defining a variable fullName that adds two variables First Name and Last Name.
 
-document.write(fullName);
+document.write(fullName); //displaying full name on site.
 
-document.write(3+3);
+document.write(3+3); // Displaying on site the sum of 3+3
