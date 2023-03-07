e1eba8db26ac5ce4f683086000018c68b5aaaed7
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-07 13:07:56 -0800

    //

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,8 @@
 var myString ="HELLLLOOO WORLD!!!";
 window.alert(myString);
 
-document.write("\" be who you are and say what you feel,"
-+ "because those who mind don\'t matter and those who matter don\'t mind. \""
-+ "-Dr.Seuss");
+var firstName="Angel";
+var lastName= "Velasco";
+var fullName= firstName + " " + lastName;
+
+document.write(fullName);
