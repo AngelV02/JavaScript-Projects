ebbb75bd88fb5e024a7d719848e18b771a35c22a
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-08 12:07:19 -0800

    added comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,7 +1,7 @@
 function text() {
-     var name ="Robert";
-     var age = 25;
-     var output = document.getElementById("output");
+     var name ="Robert"; // assigns a string value to variable name.
+     var age = 25; // assigns number vlaue to variable age.
+     var output = document.getElementById("output"); // get the html element with the ID "output"
 
-     output.innerHTML += " My name is " + name + " and I am " + age + " years old.";
+     output.innerHTML += " My name is " + name + " and I am " + age + " years old."; //displays the text contents
 }
