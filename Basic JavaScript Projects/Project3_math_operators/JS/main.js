aa22d10a15ac075e0c9987dcb906e42c04e52a4c
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-08 13:25:00 -0800

    added some mathhh

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,1 +1,11 @@
+function addition(x, y) {
+    return x + y;
+}
 
+function displayResult() {
+    var x = 3;
+    var y = 12;
+    var result = addition(x, y);
+    var output = document.getElementById("Math");
+    output.innerHTML = "The result of " + x + " x " + y + " is " + result;
+}
