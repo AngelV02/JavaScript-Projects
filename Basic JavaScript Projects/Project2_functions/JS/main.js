68e78ec414606a3a3dc0ee7b828ee21fe3e4ca12
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-08 11:43:59 -0800

    added js

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function text() {
+     var name ="Robert";
+     var age = 25;
+     var output = document.getElementById("output");
+
+     output.innerHTML = "My name is " + name + " and I am" + age + " years old.";
+}
