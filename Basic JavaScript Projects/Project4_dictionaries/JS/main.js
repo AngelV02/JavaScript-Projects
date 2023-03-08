20046684b0a1897228ef5e9811836ba77224c4db
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-08 15:25:11 -0800

    wrote dictionary assignment

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,1 +1,20 @@
+// Define the dictionary object
+var myDictionary = {
+    apple: "A fruit",
+    car: "A vehicle",
+    book: "A written work"
+  };
 
+// Define the function to output a random key-value pair from the dictionary
+function outputRandomKeyValuePair() {
+
+// Remove a random key from the dictionary using the delete operator
+var keys = Object.keys(myDictionary);
+var randomKey = keys[Math.floor(Math.random() * keys.length)];
+delete myDictionary[randomKey];
+
+
+ // Output a random key-value pair to the "myDictionary" HTML element
+var outputElement = document.getElementById("myDictionary");
+outputElement.innerHTML = "<strong>" + randomKey + ":</strong> " + myDictionary[randomKey];
+}
