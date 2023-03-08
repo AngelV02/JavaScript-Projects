cf0f25be842bbffc9e35c69fc6f40964c91ca0d9
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-08 13:33:47 -0800

    added multiplication and division functions

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,11 +1,17 @@
-function subraction(x, y) {
-    return x - y;
+function mulitplication(x, y) {
+    return x * y;
+}
+
+function division(x, y) {
+    return x / y;
 }
 
 function displayResult() {
-    var x = 15;
-    var y = 12;
-    var result = subraction(x, y);
+    var x = 10;
+    var y = 5;
+    var product = mulitplication(x, y);
+    var quotient = division(x, y);
     var output = document.getElementById("Math");
-    output.innerHTML = "The result of " + x + " - " + y + " is " + result;
+    output.innerHTML = "The product of " + x + " * " + y + " is " + product + ".<br>";
+    output.innerHTML += "The quotient of " + x + " / " + y + " is " + quotient + ".";
 }
