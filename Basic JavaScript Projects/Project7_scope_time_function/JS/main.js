2d105ebb62f4b3debbfdeb9e53eeff27b43536f1
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-09 14:02:38 -0800

    added numbers

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,1 +1,23 @@
+var x = 10;
 
+function Add_numbers_1() {
+    document.write(20 + x + "<br>");
+}
+
+function Add_numbers_2() {
+    document.write(x + 100 + "<br>");
+}
+
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_3() {
+    var y = 20;
+    document.write("<br>", 20 + y + "<br>");
+}
+function Add_numbers_4() {
+    document.write(y + 100 + "<br>");
+}
+
+Add_numbers_3();
+Add_numbers_4();
