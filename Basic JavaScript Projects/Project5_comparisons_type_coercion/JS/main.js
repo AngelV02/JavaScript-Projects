ff0aa7c520388797ef53736d5a83d9e7239d3522
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-09 08:15:38 -0800

    /

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,5 +1,8 @@
 var myVariable = "Hello, World!"; // Declare a variable and assign it a value
 
 // Use document.write() to display the variable's data type
-document.write("The data type of myVariable is: " + typeof myVariable);
+document.write("The data type of myVariable is: " + typeof String(myVariable) +"<br>");
 
+document.write(2E310);
+
+document.write(-3E310);
