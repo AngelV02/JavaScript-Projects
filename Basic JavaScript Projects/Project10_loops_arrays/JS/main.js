72060d5a2cf78114bdef0c03502030a214080a4c
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-14 11:57:08 -0700

    added const to program

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,1 +1,49 @@
+function Call_loop() {
+    var Digit ="";
+    var x = 1;
+    while (x < 11) {
+        Digit += "<br>" + x;
+        x++;
+    }
 
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+var Cars = ["BMW", "SUBARU", "FORD", "AUDI", "Kia", "Toyata"];
+var Content = "";
+var Y;
+
+function for_loop() {
+    for (Y = 0; Y < Cars.length; Y++) {
+        Content += Cars[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function array_Function() {
+    var Dog_Picture = [];
+
+    Dog_Picture[0] = "sleeping";
+    Dog_Picture[1] = "playing";
+    Dog_Picture[2] = "eating";
+    Dog_Picture[3] = "barking";
+
+    document.getElementById("Array").innerHTML = "In this picture, the dog is " +
+    Dog_Picture[2] + ".";
+}
+
+const myObject = {
+    property1: "Hello",
+    property2 : "world!"
+
+};
+
+function constant_function(){
+
+    myObject.property1 = "Hi";
+    myObject.property3 = "Im a new property!";
+
+    const element = document.getElementById("Constant");
+
+    element.innerHTML = myObject.property1 + " " + myObject.property2 + " " + myObject.property3;
+}
