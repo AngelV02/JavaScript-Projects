fa36b6e6c864b70eed381976195257fdc8f042d3
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-14 12:26:14 -0700

    added comments!

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,3 +1,5 @@
+//Defines loop function to display numbers 1 - 10 using while loop
+
 function Call_loop() {
     var Digit ="";
     var x = 1;
@@ -6,9 +8,11 @@ function Call_loop() {
         x++;
     }
 
+    // Displays the result in HTML ELEMENT wiht id Loop
     document.getElementById("Loop").innerHTML = Digit;
 }
 
+// Defines function for_loop() to display elements of array "Cars" using a for loop
 var Cars = ["BMW", "SUBARU", "FORD", "AUDI", "Kia", "Toyata"];
 var Content = "";
 var Y;
@@ -20,6 +24,7 @@ function for_loop() {
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
 
+// Defines function array_Function() to display an element of array "Dog_Picture"
 function array_Function() {
     var Dog_Picture = [];
 
@@ -32,12 +37,14 @@ function array_Function() {
     Dog_Picture[2] + ".";
 }
 
+//Defines constant object "myObject" with two properties and values
 const myObject = {
     property1: "Hello",
     property2 : "world!"
 
 };
 
+// Defines function constant_function() to modify "myObject" and display the new values in an HTML element
 function constant_function(){
 
     myObject.property1 = "Hi";
@@ -47,3 +54,17 @@ function constant_function(){
 
     element.innerHTML = myObject.property1 + " " + myObject.property2 + " " + myObject.property3;
 }
+
+// Defines object "car" with properties and a method to describe the car
+let car = {
+    make: "Subaru ",
+    model: "STI ",
+    year: "2017 ",
+    color: "white ",
+
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+
+document.getElementById("Car_Object").innerHTML = car.description();
