f5f26e16a773c3e4bf8717b0fe2acc5177bdb5b7
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-03-14 19:17:13 -0700

    added form

--- Project1/JS/JavaScript_2.js
+++ Project1/JS/JavaScript_2.js
@@ -1,1 +1,14 @@
 
+function validateForm() {
+    // Get the value of the input field
+    const inputVal = document.querySelector('#formGroupExampleInput').value;
+  
+    // Check if the input field is empty
+    if (inputVal === '') {
+      // Alert the user
+      alert('Please fill out all form fields.');
+      // Prevent the form from submitting
+      return false;
+    }
+  }
+  
